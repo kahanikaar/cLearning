5d75bca9a743b1622c8fe69f89fd97921e0b7719
Sanjiban Sengupta <b518041@iiit-bh.ac.in>
2020-05-02 08:24:04 +0530

    Update url.py

--- url.py
+++ url.py
@@ -1,1 +1,4 @@
-from django.conf.urls import urlfrom django.contrib import adminfrom ivr.views import init_automated_call, handle_user_responseurlpatterns = [   url(r'^admin/', admin.site.urls),   url(r'^gather/$', init_automated_call),   url(r'^respond/$', handle_user_response),]
+from django.conf.urls import urlfrom django.contrib 
+import adminfrom ivr.views
+import init_automated_call, handle_user_response
+urlpatterns = [   url(r'^admin/', admin.site.urls),   url(r'^gather/$', init_automated_call),   url(r'^respond/$', handle_user_response),]
